e1d34c88307411dc919b129b9e043340d0c19d83
Kdau <126079940+izonnnoui@users.noreply.github.com>
2026-02-19 01:33:01 +0100

    courage

---
+++ src/entities/torch.gd
@@ -0,0 +1,23 @@
+extends OmniLight3D
+
+@export var vitesse_flamme = 2.0
+@export var intensite_flamme = 0.2
+@export var instabilite = 0.2
+
+var base_energy = 1.0
+var base_range = 8.0
+var cible_energy = 1.0
+
+func _ready():
+	base_energy = light_energy
+	base_range = omni_range
+	cible_energy = base_energy
+
+func _process(delta):
+	if abs(light_energy - cible_energy) < 0.1:
+		cible_energy = base_energy + randf_range(-intensite_flamme, intensite_flamme)
+
+	light_energy = lerp(light_energy, cible_energy, vitesse_flamme * delta)
+	
+	var tremblement = randf_range(-instabilite, instabilite)
+	omni_range = ((base_range * $'../../../../..'.current_life * 0.5) * (light_energy / base_energy)) + tremblement
